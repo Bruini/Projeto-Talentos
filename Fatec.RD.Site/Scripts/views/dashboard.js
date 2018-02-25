deb9f60f11f3b4e89fcfaa973b4eea26cde40bbb
Pedro Bruini <pedro.bruini@hotmail.com>
2018-02-25 19:05:45 -0300

    Front Dashboard
    -Front-end funcional com as funções básicas

--- Fatec.RD.Site/Scripts/views/dashboard.js
+++ Fatec.RD.Site/Scripts/views/dashboard.js
@@ -4,12 +4,17 @@ var api = "http://localhost:63649/api/Despesa/";
 
 var valorTotal = $("#valorTotal");
 var qtdDespesas = $("#qtdDespesas");
+var valorTotalRelatorio = $("#valorTotalRelatorio");
+var qtdDespesasRelatorio = $("#qtdDespesasRelatorio");
 
+var selectRelatorio = $("#listaRelatorios");
 
 somaTotal();
-
+getRelatorios();
 /***************************************************************/
 
+
+/*********** Funções da API ***************/
 function somaTotal() {
    
     $.ajax({
@@ -26,7 +31,69 @@ function somaTotal() {
     });
 }
 
+function getRelatorios() {
+    var api = "http://localhost:63649/api/Relatorio/";
+    $.ajaxSetup({ async: false });
+    var retorno;
+    $.ajax({
+        type: 'GET',
+        url: api,
+        success: function (relatorios) {
+            retorno = relatorios;
+            $.each(retorno, function (i, item) {
+
+
+                selectRelatorio.append($('<option>', {
+                    value: item.Id,
+                    text: item.Descricao
+                }));
+
+            });
+        },
+        error: function (error) {
+            console.log(error);
+        }
+    });
+    $.ajaxSetup({ async: true });
+
+}
+
+function somaTotalRelatorio(id) {
+
+    $.ajax({
+        type: "GET",
+        url: api + "SomaTotal/" + id,
+        success: function (data) {
+
+            prencheSomaDespesasRelatorio(data);
+
+        },
+        error: function (error) {
+            console.log(error);
+        }
+    });
+}
+
+/********************************************/
+
+
+
+/*********** Funções Internas ***************/
+
 function prencheSomaDespesas(data) {
     valorTotal.text(data.ValorTotal);
     qtdDespesas.text(data.Despesas);
 }
+
+function prencheSomaDespesasRelatorio(data) {
+    valorTotalRelatorio.text(data.ValorTotal);
+    qtdDespesasRelatorio.text(data.Despesas);   
+}
+
+
+
+selectRelatorio.on('change', function () {
+    somaTotalRelatorio(this.value);
+})
+
+/********************************************/
